4b8e709250db441693e10b18ef86679021209121
Lorenzo Murarotto <lnzmrr@gmail.com>
2020-11-16 21:13:52 +0000

    more clean up

--- src/wheel.rs
+++ src/wheel.rs
@@ -2,8 +2,6 @@ pub struct Wheel {
     pub size: u64,
     pub basis: Vec<u64>,
 
-    basis_index: usize,
-
     distances: Vec<u64>,
     index: usize,
     value: u64,
@@ -11,11 +9,11 @@ pub struct Wheel {
 
 impl Wheel {
     pub fn new(basis: Vec<u64>) -> Self {
-        let size = basis.iter().product::<u64>();
+        let size = basis.iter().product();
         let is_coprime = |n: &u64| basis.iter().all(|b| n % b != 0);
         let spokes = (1..=size + 1).filter(is_coprime).collect::<Vec<_>>();
         let distances = spokes.windows(2).map(|ss| ss[1] - ss[0]).collect();
-        Wheel { basis, size, basis_index: 0, distances, index: 0, value: 1 }
+        Wheel { basis, size, distances, index: 0, value: 1 }
     }
 }
 
@@ -23,14 +21,9 @@ impl Iterator for Wheel {
     type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.basis_index < self.basis.len() {
-            self.basis_index += 1;
-            Some(self.basis[self.basis_index - 1])
-        } else {
-            self.value += self.distances[self.index];
-            self.index = (self.index + 1) % self.distances.len();
-            Some(self.value)
-        }
+        self.value += self.distances[self.index];
+        self.index = (self.index + 1) % self.distances.len();
+        Some(self.value)
     }
 }
 
@@ -46,8 +39,8 @@ mod tests {
         assert_eq!(
             wheel.take_while(|n| *n < 100).collect::<Vec<_>>(),
             [
-                2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59, 61, 67, 71, 73,
-                77, 79, 83, 89, 91, 97
+                7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59, 61, 67, 71, 73, 77, 79,
+                83, 89, 91, 97
             ]
         )
     }
@@ -55,7 +48,7 @@ mod tests {
     #[bench]
     fn bench_wheel_1_000_000(b: &mut Bencher) {
         b.iter(move || {
-            let mut wheel = Wheel::new(vec![2, 3, 5]);
+            let mut wheel = Wheel::new(vec![2, 3, 5, 7]);
             let _ = wheel.by_ref().take_while(|n| *n < 1_000_000).for_each(drop);
         });
     }
@@ -63,7 +56,7 @@ mod tests {
     #[bench]
     fn bench_wheel_10_000_000(b: &mut Bencher) {
         b.iter(move || {
-            let mut wheel = Wheel::new(vec![2, 3, 5]);
+            let mut wheel = Wheel::new(vec![2, 3, 5, 7]);
             let _ = wheel.by_ref().take_while(|n| *n < 10_000_000).for_each(drop);
         });
     }
